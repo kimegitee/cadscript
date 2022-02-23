5fe96d825bca51fa4d4b6903de0ee217c105e358
Nhat Bui (Nick) <kimegitee@gmail.com>
2022-02-24 03:42:46 +0700

    Excecute eagerly (#1)
    * Work around sending utf-16 strings
    
    * Correctly send lines

--- cadscript/core.py
+++ cadscript/core.py
@@ -1,5 +1,8 @@
+from pexpect.popen_spawn import PopenSpawn
+from pathlib import Path
+import os
+import signal
 import tempfile
-import subprocess
 
 def run_script(script:str, verbose=True):
     '''Excecute a series of AutoCAD commands in a subprocess
@@ -11,20 +14,23 @@ def run_script(script:str, verbose=True):
         file.close() # On Windows file has to be closed before being opened again
         subprocess.run(['accoreconsole.exe', '/s', file.name], capture_output=not verbose)
 
-class Command:
+class Program:
     '''Wrapper to generate AutoCAD script
 
     Each method corresponds to an AutoCAD command of the same name, except when
     such commands conflict with Python keywords, such as `import_` below.
     '''
-    def __init__(self, s:str=''):
-        self.s = s
+    def __init__(self, s=[]):
+        self.p = PopenSpawn('accoreconsole.exe', encoding='utf-16-le')
+        self.p.expect('.*Command:.*:.*:.*', timeout=None)
+        print(self.p.after)
+        self.terminated = False
     def __enter__(self):
         return self
     def __exit__(self, *_):
-        self.exec(verbose=True)
+        self.terminate()
     def _append(self, s:str):
-        self.s += s
+        self.s.append(s)
         return self
     def _cancel_sequence(self):
         '''Add sequence to end infinite input loop'''
@@ -36,17 +42,42 @@ class Command:
         self._append(f'EXPORT "{path}" all\n\n')
         return self._cancel_sequence()
     def open(self, path:str):
-        return self._append(f'OPEN "{path}"\n')
-    def to_dxf(self, path:str=None):
+        return self._exec_utf8(f'open "{path}"\n')
+    def to_dxf(self, path:str=None, overwrite=False):
         path = '' if path is None else f'"{path}"'
-        return self._append(f'SAVEAS DXF 16 {path}\n')
+        overwrite = 'Y' if overwrite else 'N'
+        return self._exec_utf8(f'saveas dxf 16 {path} {overwrite}\n')
     def stlout(self, path:str):
         return self._append(f'STLOUT all\n\n\n"{path}"\n(command)\n')
     def erase(self):
         return self._append('ERASE all\n\n')
     def purge(self):
         return self._append(f'-PURGE all * n\n')
-    def exec(self, verbose=True):
-        '''Execute underlying script'''
-        s, self.s = self.s, ''
-        run_script(s, verbose)
+    def terminate(self):
+        self.p.kill(signal.SIGTERM)
+        self.terminated = True
+    def _return(self, command):
+        command = command.split(' ')[0]
+        ok = f'Command: {command}.*Command:'
+        confirm = f'Command: {command}.*<[YN]>.*'
+        error = '\**MessageBox.*'
+        status = self.p.expect([ok, confirm, error], timeout=3)
+        if status == -1:
+            raise(RuntimeError('\n' + self.p.after))
+        else:
+            print(self.p.after)
+        return self
+    def _assert_running(self):
+        assert not self.terminated, 'Program has already been terminated'
+    def _exec(self, command):
+        self._assert_running()
+        self.p.send(command)
+        return self._return(command)
+    def _exec_utf8(self, command):
+        '''Send utf8 encoded command to AutoCAD for execution'''
+        self._assert_running()
+        with tempfile.NamedTemporaryFile('w+', suffix='.scr', delete=False, encoding='utf-8-sig') as file:
+            file.write(command)
+            file.close() # On Windows file has to be closed before being opened again
+            self.p.send(f'script "{file.name}"\n')
+            return self._return(command)
